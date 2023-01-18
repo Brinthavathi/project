c6f73c5f589df7e0e91ec77bd35090883c478efc
Admin <admin@Admins-MacBook-Air-4.local>
2023-01-18 13:02:50 +0530

    adding js file.

--- javascript program/index.js
+++ javascript program/index.js
@@ -1,1 +1,8 @@
-window.addEventListener('DOMContentlocated')
+const scriptURL="https://script.google.com/macros/s/AKfycbxkgWotCMyvIKoT9aGtPCndZdHtHrq8i972t9CKAVjo6xC7T0I/exec"
+const form = document.forms['google-sheet']
+
+form.addEventListener('submit',e => {e.preventDefault(
+	fetch(scriptURL,{method:'post',body:new formData(form)})
+	.then(response => alert("thanks for contracting us...! we will contact you soon..."))
+	.catch(error => console.error('error!',error.message))
+})
